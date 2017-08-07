f4923262d0641f19c8a49cc761b743d47a5daa35
JackSpera <giovanni.spera2011@libero.it>
2017-08-07 02:13:31 +0200

    Update LogFile.cs

--- Log.cs
+++ Log.cs
@@ -1,4 +1,15 @@
 namespace Logger{
 	class Log{
+		private LogFile Parent;
+
+		public LogType Type;
+
+		public string Data;
+
+		public Log(LogFile parent, LogType type, string data){
+			this.Parent = parent;
+			this.Type = type;
+			this.Data = data;
+		}
 	}
 }
--- LogFile.cs
+++ LogFile.cs
@@ -7,14 +7,34 @@ namespace Logger {
 		public char OpenBracket = '[';
 		public char CloseBracket = ']';
 
-		//TODO
-		private SecureLevel Lock = SecureLevel.PUBLIC;
+		private SecureLevel Lock;
 
-		public List<Log> Data = new List<Log>();
+		public List<Log> Data {
+			get{
+				if (Lock == SecureLevel.SEE_ONLY || Lock == SecureLevel.ALL)
+					return Data;	
+
+				return null;
+			}
+
+
+
+			set{
+				if (Lock == SecureLevel.SET_ONLY || Lock == SecureLevel.ALL)
+					Data = value;
+
+				return;
+			}
+		}
+
+		public string Format = "{Parent.OpenBracket}{Date}{Parent.CloseBracket}{Parent.OpenBracket}{LogType}{Parent.CloseBracket}{Data}";
+
 
 		public LogFile(string filePath, SecureLevel lockLevel=SecureLevel.SEE_ONLY){
 			this.FilePath = filePath;
 			this.Lock = lockLevel;
+
+			this.Data = new List<Log>();
 		}
 	}
 }
